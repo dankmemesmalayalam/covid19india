74940b69c3806d9390f7287b70558dbbfd845e1d
HAL 9000 <info@dmmcollective.xyz>
2020-03-26 11:01:07 +0530

    Create contribute.js

---
+++ src/components/contribute.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react';
+
+function Contribute(props) {
+  useEffect(()=>{
+    window.location.replace('https://forms.gle/ySAcTQai2cwsURqy6');
+  });
+
+  return (
+    <div></div>
+  );
+}
+
+export default Contribute;
